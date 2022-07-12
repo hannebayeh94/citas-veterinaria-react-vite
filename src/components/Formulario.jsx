a1f20b40440d4c9b519f007ad2211962b7d30ee1
Hanne Bayeh <hannejosebayehvillarreal@gmail.com>
2022-07-12 13:44:40 -0500

    cambio de titulo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -80,7 +80,7 @@ const Formulario = ({ setPacientes, pacientes, paciente,setPaciente }) => {
 
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
-      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes Animales</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Add Pacientes y{" "}
         <span className="text-indigo-600 font-bold ">Administralos</span>
